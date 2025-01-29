60d122526c79f8ba5008a2ad14faed44e8d79789
shooooooma415 <shoma20040415@icloud.com>
2025-01-30 01:53:53 +0900

    feat:インターフェースを作成

--- repository/auth/auth.go
+++ repository/auth/auth.go
@@ -1,1 +1,8 @@
 package auth
+
+import "painteer/model"
+
+type UsersRepository interface {
+	CreateUser(user model.CreateUser) (*model.UserId, error)
+	SignInUser(authId model.AuthId) (*model.UserId, error)
+}
