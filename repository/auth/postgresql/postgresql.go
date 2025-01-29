bee5555457a1cc865d6baec25483ebca5d8cec7b
shooooooma415 <shoma20040415@icloud.com>
2025-01-30 01:29:50 +0900

    feat:CreateUserQueryの実装

--- repository/auth/postgresql/postgresql.go
+++ repository/auth/postgresql/postgresql.go
@@ -2,6 +2,8 @@ package postgresql
 
 import (
 	"database/sql"
+	"fmt"
+	"painteer/model"
 )
 
 type AuthRepositoryImpl struct {
@@ -10,4 +12,21 @@ type AuthRepositoryImpl struct {
 
 func NewAuthRepository(db *sql.DB) *AuthRepositoryImpl {
 	return &AuthRepositoryImpl{DB: db}
+}
+
+func (q *AuthRepositoryImpl) CreateUserQuery(createUser model.CreateUser) (*model.UserId, error) {
+	query := `
+		INSERT INTO users (name,icon,auth_id) 
+		VALUES ($1,$2,$3)
+		RETURNING id, name
+	`
+
+	var userId model.UserId
+	var userName model.UserName
+
+	err := q.DB.QueryRow(query, createUser).Scan(&userId, &userName)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create user: %w", err)
+	}
+	return &userId, nil
 }
