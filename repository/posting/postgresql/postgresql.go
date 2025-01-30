83528222ac3477e780ecfc2da5a250834f8c8fcd
shooooooma415 <shoma20040415@icloud.com>
2025-01-30 18:15:11 +0900

    feat: NewPostingRepositoryを追加

--- repository/posting/postgresql/postgresql.go
+++ repository/posting/postgresql/postgresql.go
@@ -1,1 +1,14 @@
 package postgresql
+
+import (
+	"database/sql"
+	"painteer/model"
+)
+
+type PostingRepositoryImpl struct {
+	DB *sql.DB
+}
+
+func NewPostingRepository(db *sql.DB) *PostingRepositoryImpl {
+	return &PostingRepositoryImpl{DB: db}
+}
