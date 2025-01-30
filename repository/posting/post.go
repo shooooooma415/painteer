f8990be8d157e4609e16fb4b0a3e0acda4a9fad5
shooooooma415 <shoma20040415@icloud.com>
2025-01-30 20:01:39 +0900

    fix:インターフェースの返り値を修正

--- repository/posting/post.go
+++ repository/posting/post.go
@@ -4,6 +4,6 @@ import "painteer/model"
 
 type PostingsRepository interface {
 	UploadPost(uploadPost model.UploadPost) (*model.Post, error)
-	DeletePost(postId model.PostId) (*model.PostId, error)
+	DeletePost(postId model.PostId) (*model.Post, error)
 	SelectPost(selectPost model.SelectPost)(model.Post,error)
 }
