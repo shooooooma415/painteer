866892b27f72d25f7f673d071ee70354ae70962d
shooooooma415 <shoma20040415@icloud.com>
2025-01-30 12:02:24 +0900

    fix:FindUserByIDの返り値を変更

--- repository/auth/postgresql/postgresql.go
+++ repository/auth/postgresql/postgresql.go
@@ -23,30 +23,45 @@ func (q *AuthRepositoryImpl) CreateUser(createUser model.CreateUser) (*model.Use
 
 	var resultUser model.User
 
-	err := q.DB.QueryRow(query, createUser.UserName, createUser.Icon, createUser.AuthId).Scan(&resultUser.UserId, &resultUser.UserName,&resultUser.AuthId,&resultUser.Icon)
+	err := q.DB.QueryRow(
+		query,
+		createUser.UserName,
+		createUser.Icon,
+		createUser.AuthId,
+	).Scan(
+		&resultUser.UserId,
+		&resultUser.UserName,
+		&resultUser.AuthId,
+		&resultUser.Icon,
+	)
+
 	if err != nil {
 		return nil, fmt.Errorf("failed to create user: %w", err)
 	}
 	return &resultUser, nil
 }
 
-func (q *AuthRepositoryImpl) FindUserByID(authId model.AuthId) (*model.UserId, error) {
+func (q *AuthRepositoryImpl) FindUserByID(authId model.AuthId) (*model.User, error) {
 	query := `
-		SELECT user_id, auth_id
+		SELECT *
 		FROM user
 		WHERE auth_id = $1
 	`
 
-	var resultUserId model.UserId
-	var resultAuthId model.AuthId
+	var resultUser model.User
 
-	err := q.DB.QueryRow(query, authId).Scan(&resultUserId, &resultAuthId)
+	err := q.DB.QueryRow(
+		query,
+		authId,
+	).Scan(
+		&resultUser.UserId,
+		&resultUser.UserName,
+		&resultUser.AuthId,
+		&resultUser.Icon,
+	)
+
 	if err != nil {
 		return nil, fmt.Errorf("failed to create user: %w", err)
 	}
-	if resultAuthId != authId {
-		return nil, fmt.Errorf("auth_id mismatch: expected %v, got %v", authId, resultAuthId)
-	}
-	return &resultUserId, nil
+	return &resultUser, nil
 }
-
