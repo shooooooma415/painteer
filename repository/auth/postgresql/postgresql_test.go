707972c160f00d274403e0cb1039ecd69ddbce11
shooooooma415 <shoma20040415@icloud.com>
2025-01-30 12:09:38 +0900

    fix:TestCreateUserの中身を変更

--- repository/auth/postgresql/postgresql_test.go
+++ repository/auth/postgresql/postgresql_test.go
@@ -37,24 +37,36 @@ func TestCreateUser(t *testing.T) {
 			repository := postgresql.NewAuthRepository(db)
 			got, err := repository.CreateUser(tc.createUser)
 
+			want := model.User{
+				UserName: tc.createUser.UserName,
+				Icon:     tc.createUser.Icon,
+				AuthId:   tc.createUser.AuthId,
+			}
+
 			if err != nil {
 				t.Fatalf("CreateUserQuery() error = %v", err)
 			}
-			
-			if got == nil {
-				t.Fatal("CreateUserQuery() returned nil userId, expected valid userId")
+
+			if got.UserName != want.UserName {
+				t.Errorf("CreateUser() UserName = %v, want %v", got.UserName, want.UserName)
 			}
-			
+			if got.AuthId != want.AuthId {
+				t.Errorf("CreateUser() AuthId = %v, want %v", got.AuthId, want.AuthId)
+			}
+			if got.Icon != want.Icon {
+				t.Errorf("CreateUser() Icon = %v, want %v", got.Icon, want.Icon)
+			}
+
 		})
 	}
 }
 
-func TestFindUserByID(t *testing.T){
-	testCases :=[]struct{
-		name string
+func TestFindUserByID(t *testing.T) {
+	testCases := []struct {
+		name   string
 		authId model.AuthId
 	}{
-		name:"authIdを元に正常にuserIdを取得する",
-		authId:"hoge",
+		name:   "authIdを元に正常にuserIdを取得する",
+		authId: "hoge",
 	}
 }
