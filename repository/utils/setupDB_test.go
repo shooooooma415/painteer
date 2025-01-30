ad4737a66f7bf2f371d3689597c908ced8ca0bb8
shooooooma415 <shoma20040415@icloud.com>
2025-01-30 10:21:33 +0900

    feat:setupDBの実装

--- repository/utils/setupDB_test.go
+++ repository/utils/setupDB_test.go
@@ -1,1 +1,19 @@
 package utils_test
+
+import (
+	setupDB "painteer/repository/utils"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+func TestConnectDB(t *testing.T) {
+	db, err := setupDB.ConnectDB()
+	if err != nil {
+		t.Fatalf("Failed to connect to the database: %v", err)
+	}
+	if db == nil {
+		t.Fatal("Expected a valid database connection, but got nil")
+	}
+	defer db.Close()
+}
