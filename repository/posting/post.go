ba44b752726a501e8282fb152f87aa21f190f06c
shooooooma415 <shoma20040415@icloud.com>
2025-02-02 21:20:46 +0900

    fix:インターフェースの命名を修正

--- repository/posting/post.go
+++ repository/posting/post.go
@@ -3,7 +3,7 @@ package post
 import "painteer/model"
 
 type PostingsRepository interface {
-	UploadPost(uploadPost model.UploadPost) (*model.PostId, error)
+	CreatePost(uploadPost model.UploadPost) (*model.PostId, error)
 	DeletePost(postId model.PostId) (*model.PostId, error)
 	FetchPost(postId model.PostId) (*model.Post, error)
 }
