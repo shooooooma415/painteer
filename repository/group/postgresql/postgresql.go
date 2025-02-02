2d01a59755fb405261274065c33046b4f0030545
shooooooma415 <shoma20040415@icloud.com>
2025-02-02 22:37:47 +0900

    feat:repositoryを定義

--- repository/group/postgresql/postgresql.go
+++ repository/group/postgresql/postgresql.go
@@ -1,2 +1,12 @@
 package postgresql
 
+import "database/sql"
+
+type GroupRepositoryImpl struct {
+	DB *sql.DB
+}
+
+func NewGroupRepository(db *sql.DB) *GroupRepositoryImpl {
+	return &GroupRepositoryImpl{DB: db}
+}
+
