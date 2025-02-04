4686dd0e0f872c9eba6ab1305758fd4589688601
shooooooma415 <shoma20040415@icloud.com>
2025-02-04 17:23:14 +0900

    feat:IsUserExistをinterfaceに追加

--- repository/group/group.go
+++ repository/group/group.go
@@ -5,6 +5,7 @@ import "painteer/model"
 type GroupRepository interface {
 	CreateGroup(createGroup model.Group) (*model.Group, error)
 	VerifyPassword(VerifyPassword model.VerifyPassword)(*model.GroupId,error)
+	IsUserExist(joinGroup model.JoinGroup)(bool,error)
 	JoinGroup(joinGroup model.JoinGroup) (*model.Group, error)
 	FetchGroup(userId model.UserId) (*model.FetchedGroup, error)
 	InsertPublicSetting(publicSetting model.PublicSetting) (*model.PublicSetting, error)
