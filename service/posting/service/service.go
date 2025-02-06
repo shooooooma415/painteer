2fe910eafe299c93c2ec8d322432f176b009385b
shooooooma415 <shoma20040415@icloud.com>
2025-02-07 03:16:03 +0900

    feat:repoを継承

--- service/posting/service/service.go
+++ service/posting/service/service.go
@@ -1,1 +1,26 @@
 package service
+
+import (
+	"painteer/model"
+	"painteer/repository/posting"
+)
+
+type PostingsServiceImpl struct {
+	repo post.PostingsRepository
+}
+
+func NewPostingService(repo post.PostingsRepository) *PostingsServiceImpl {
+	return &PostingsServiceImpl{repo: repo}
+}
+
+func (s *PostingsServiceImpl) CreatePost(uploadPost model.UploadPost) (*model.PostId, error){
+	return s.repo.CreatePost(uploadPost)
+}
+
+func (s *PostingsServiceImpl)DeletePost(postId model.PostId) (*model.PostId, error){
+	return s.repo.DeletePost(postId)
+}
+
+func (s *PostingsServiceImpl)GetPostByID(postId model.PostId) (*model.Post, error){
+	return s.repo.FindPostByID(postId)
+}
