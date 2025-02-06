f3a47f18969c1f42d4f80c037124321354d597fa
shooooooma415 <shoma20040415@icloud.com>
2025-02-06 20:57:17 +0900

    fix:命名を修正

--- service/auth/auth.go
+++ service/auth/auth.go
@@ -2,7 +2,7 @@ package auth
 
 import "painteer/model"
 
-type UsersService interface {
+type AuthService interface {
 	RegisterUser(user model.CreateUser) (*model.User, error)
 	AuthenticateUser(authId model.AuthId) (*model.User, error)
 	GetUserByID(userId model.UserId) (*model.User, error)
--- service/auth/service/service.go
+++ service/auth/service/service.go
@@ -5,22 +5,22 @@ import (
 	"painteer/repository/auth"
 )
 
-type UsersServiceImpl struct {
+type AuthServiceImpl struct {
 	repo auth.UsersRepository
 }
 
-func NewUsersService(repo auth.UsersRepository) *UsersServiceImpl {
-	return &UsersServiceImpl{repo: repo}
+func NewAuthService(repo auth.UsersRepository) *AuthServiceImpl {
+	return &AuthServiceImpl{repo: repo}
 }
 
-func (s *UsersServiceImpl) RegisterUser(user model.CreateUser) (*model.User, error) {
+func (s *AuthServiceImpl) RegisterUser(user model.CreateUser) (*model.User, error) {
 	return s.repo.CreateUser(user)
 }
 
-func (s *UsersServiceImpl) AuthenticateUser(authId model.AuthId) (*model.User, error) {
+func (s *AuthServiceImpl) AuthenticateUser(authId model.AuthId) (*model.User, error) {
 	return s.repo.SignInUser(authId)
 }
 
-func (s *UsersServiceImpl) GetUserByID(userId model.UserId) (*model.User, error) {
+func (s *AuthServiceImpl) GetUserByID(userId model.UserId) (*model.User, error) {
 	return s.repo.FindUserByUserID(userId)
 }
