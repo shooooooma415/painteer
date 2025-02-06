bbaae18ae9366a382324dfe99b8607f2f3cc64f6
shooooooma415 <shoma20040415@icloud.com>
2025-02-07 03:23:59 +0900

    feat:GetPostsByPrefectureIDAndGroupIDsの実装

--- service/posting/service/service.go
+++ service/posting/service/service.go
@@ -1,8 +1,9 @@
 package service
 
 import (
+	"fmt"
 	"painteer/model"
-	"painteer/repository/posting"
+	post "painteer/repository/posting"
 )
 
 type PostingsServiceImpl struct {
@@ -13,14 +14,41 @@ func NewPostingService(repo post.PostingsRepository) *PostingsServiceImpl {
 	return &PostingsServiceImpl{repo: repo}
 }
 
-func (s *PostingsServiceImpl) CreatePost(uploadPost model.UploadPost) (*model.PostId, error){
+func (s *PostingsServiceImpl) CreatePost(uploadPost model.UploadPost) (*model.PostId, error) {
 	return s.repo.CreatePost(uploadPost)
 }
 
-func (s *PostingsServiceImpl)DeletePost(postId model.PostId) (*model.PostId, error){
+func (s *PostingsServiceImpl) DeletePost(postId model.PostId) (*model.PostId, error) {
 	return s.repo.DeletePost(postId)
 }
 
-func (s *PostingsServiceImpl)GetPostByID(postId model.PostId) (*model.Post, error){
+func (s *PostingsServiceImpl) GetPostByID(postId model.PostId) (*model.Post, error) {
 	return s.repo.FindPostByID(postId)
+}
+
+func (s *PostingsServiceImpl) GetPostsByPrefectureIDAndGroupIDs(prefectureIDAndGroupIDs model.PrefectureIDAndGroupIDs) (*model.Posts, error) {
+	var posts []model.Post
+
+	for _, groupId := range prefectureIDAndGroupIDs.GroupIds {
+		prefectureIDAndGroupID := model.PrefectureIDAndGroupID{
+			PrefectureId: prefectureIDAndGroupIDs.PrefectureId,
+			GroupId:      groupId,
+		}
+
+		post, err := s.repo.FindPostByPrefectureIDAndGroupID(prefectureIDAndGroupID)
+		if err != nil {
+			fmt.Printf("Error fetching post for PrefectureId %v and GroupId %v: %v\n", prefectureIDAndGroupIDs.PrefectureId, groupId, err)
+			continue
+		}
+
+		if post != nil {
+			posts = append(posts, *post)
+		}
+	}
+
+	if len(posts) == 0 {
+		return &model.Posts{Posts: []model.Post{}}, nil
+	}
+
+	return &model.Posts{Posts: posts}, nil
 }
