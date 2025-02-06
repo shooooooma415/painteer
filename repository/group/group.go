10f78af7cfecaef470547e35eba71072c064d6a4
shooooooma415 <shoma20040415@icloud.com>
2025-02-07 01:23:43 +0900

    delete:FindPasswordAndGroupNameを削除

--- repository/group/group.go
+++ repository/group/group.go
@@ -4,7 +4,6 @@ import "painteer/model"
 
 type GroupRepository interface {
 	CreateGroup(createGroup model.Group) (*model.Group, error)
-	FindPasswordAndGroupName() (*model.PasswordAndNames, error)
 	InsertGroup(insertGroup model.InsertGroup) (*model.Group, error)
 	FindUserGroupsByUserID(userId model.UserId) (*model.UserGroups, error)
 	FindGroupByGroupID(groupId model.GroupId) (*model.Group, error)
