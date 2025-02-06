460ea2f8fafa3db386233217f10e29de0c3b045d
shooooooma415 <shoma20040415@icloud.com>
2025-02-07 02:23:40 +0900

    feat:interfaceの追加

--- repository/posting/post.go
+++ repository/posting/post.go
@@ -6,4 +6,5 @@ type PostingsRepository interface {
 	CreatePost(uploadPost model.UploadPost) (*model.PostId, error)
 	DeletePost(postId model.PostId) (*model.PostId, error)
 	FindPostByID(postId model.PostId) (*model.Post, error)
+	FindPostByPrefectureIDAndGroupID()(*model.Post,error)
 }
