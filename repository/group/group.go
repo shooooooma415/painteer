424976badd5387459759b24fa675879352704055
shooooooma415 <shoma20040415@icloud.com>
2025-02-06 21:35:11 +0900

    fix:Join->Insertに修正

--- repository/group/group.go
+++ repository/group/group.go
@@ -6,7 +6,7 @@ type GroupRepository interface {
 	CreateGroup(createGroup model.Group) (*model.Group, error)
 	VerifyPassword(VerifyPassword model.VerifyPassword) (*model.GroupId, error)
 	IsUserExist(joinGroup model.JoinGroup) (bool, error)
-	JoinGroup(joinGroup model.JoinGroup) (*model.Group, error)
+	InsertGroup(joinGroup model.JoinGroup) (*model.Group, error)
 	FindUserGroupsByUserID(userId model.UserId) (*model.UserGroups, error)
 	FindGroupByGroupID(groupId model.GroupId) (*model.Group, error)
 	FindGroupMembersByGroupID(groupId model.GroupId) (*model.GroupMembers, error)
