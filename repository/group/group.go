1ae216828878dc41bb508b203f4740499aa05a30
shooooooma415 <shoma20040415@icloud.com>
2025-02-08 02:47:51 +0900

    feat:メソッドを定義

--- repository/group/group.go
+++ repository/group/group.go
@@ -3,7 +3,7 @@ package group
 import "painteer/model"
 
 type GroupRepository interface {
-	CreateGroup(createGroup model.Group) (*model.Group, error)
+	CreateGroup(createGroup model.CreateGroup) (*model.Group, error)
 	CreateUserGroup(CreateUserGroup model.CreateUserGroup) (*model.CreateUserGroup, error)
 	FindUserGroupsByUserID(userId model.UserId) (*model.UserGroups, error)
 	FindGroupByGroupID(groupId model.GroupId) (*model.Group, error)
--- service/group.go
+++ service/group.go
@@ -6,7 +6,7 @@ import (
 )
 
 type GroupService interface {
-	RegisterGroup(createGroup model.Group) (*model.Group, error)
+	RegisterGroup(Group model.CreateGroup) (*model.Group, error)
 	JoinGroup(CreateUserGroup model.CreateUserGroup) (*model.CreateUserGroup, error)
 	GetUserGroupSummaryByUserID(userId model.UserId) ([]model.GroupSummary, error)
 	GetGroupMembersByGroupID(groupId model.GroupId) (*model.GroupMembers, error)
@@ -20,4 +20,24 @@ type GroupServiceImpl struct {
 
 func NewAuthService(repo group.GroupRepository) *GroupServiceImpl {
 	return &GroupServiceImpl{repo: repo}
+}
+
+func (s *GroupServiceImpl) RegisterGroup(Group model.CreateGroup) (*model.Group, error) {
+	return s.repo.CreateGroup(Group)
+}
+
+func (s *GroupServiceImpl) JoinGroup(CreateUserGroup model.CreateUserGroup) (*model.CreateUserGroup, error) {
+
+}
+
+func (s *GroupServiceImpl) GetUserGroupSummaryByUserID(userId model.UserId) ([]model.GroupSummary, error) {
+
+}
+
+func (s *GroupServiceImpl) GetGroupMembersByGroupID(groupId model.GroupId) (*model.GroupMembers, error) {
+	return s.repo.FindGroupMembersByGroupID(groupId)
+}
+
+func (s *GroupServiceImpl) RegisterPublicSetting(publicSetting model.PublicSetting) (*model.PublicSetting, error) {
+	return s.repo.CreatePublicSetting(publicSetting)
 }
