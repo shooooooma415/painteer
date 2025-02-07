4851654bc284fbdf8c864edcdb19f6a9e66306d6
shooooooma415 <shoma20040415@icloud.com>
2025-02-08 01:23:52 +0900

    fix:interfaceの返り値を修正

--- service/posting.go
+++ service/posting.go
@@ -10,7 +10,7 @@ type PostingService interface {
 	CreatePost(uploadPost model.UploadPost) (*model.PostId, error)
 	DeletePost(postId model.PostId) (*model.PostId, error)
 	GetPostByID(postId model.PostId) (*model.Post, error)
-	GetPostsByPrefectureIDAndGroupIDs(prefectureIDAndGroupIDs model.PrefectureIDAndGroupIDs) (*model.Posts, error)
+	GetPostsByPrefectureIDAndGroupIDs(prefectureIDAndGroupIDs model.PrefectureIDAndGroupIDs) ([]model.Post, error)
 }
 
 type PostingsServiceImpl struct {
