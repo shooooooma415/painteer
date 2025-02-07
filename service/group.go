4e8c371ce16374dbd7016988783664d58dda3520
shooooooma415 <shoma20040415@icloud.com>
2025-02-08 02:37:21 +0900

    feat:func NewAuthServiceを追加

--- service/group.go
+++ service/group.go
@@ -1,6 +1,9 @@
 package service
 
-import "painteer/model"
+import (
+	"painteer/model"
+	"painteer/repository/group"
+)
 
 type GroupService interface {
 	RegisterGroup(createGroup model.Group) (*model.Group, error)
@@ -9,4 +12,12 @@ type GroupService interface {
 	GetGroupMembersByGroupID(groupId model.GroupId) (*model.GroupMembers, error)
 	GetGroupSummaryByGroupID(groupId model.GroupId) (*model.GroupSummary, error)
 	RegisterPublicSetting(publicSetting model.PublicSetting) (*model.PublicSetting, error)
+}
+
+type GroupServiceImpl struct {
+	repo group.GroupRepository
+}
+
+func NewAuthService(repo group.GroupRepository) *GroupServiceImpl {
+	return &GroupServiceImpl{repo: repo}
 }
