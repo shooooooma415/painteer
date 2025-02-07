1361336414c9d126ade439c1db6ccfb99e96a6ec
shooooooma415 <shoma20040415@icloud.com>
2025-02-08 03:53:03 +0900

    test:よしなに作成

--- repository/paint/postgresql/postgresql_test.go
+++ repository/paint/postgresql/postgresql_test.go
@@ -14,34 +14,54 @@ import (
 	_ "github.com/lib/pq"
 )
 
-func TestCreateUserAndPostAndFetchPostCount(t *testing.T) {
+func TestFindPostIDsByPrefecture(t *testing.T) {
 	testCases := []struct {
 		name       string
 		createUser model.CreateUser
-		uploadPost model.UploadPost
-		want       model.Count
+		uploadPosts []model.UploadPost
+		want       []model.PostsByPrefecture
 	}{
 		{
-			name: "ユーザーの作成＆画像の投稿->投稿数を取得",
+			name: "ユーザーの作成＆複数の投稿->都道府県ごとの投稿IDリストを取得",
 			createUser: model.CreateUser{
 				UserName: "hoge",
 				Icon:     "hoge",
 				AuthId:   "hogehogehogehoge",
 			},
-			uploadPost: model.UploadPost{
-				Image:        "hoge",
-				Date:         time.Date(2025, time.January, 30, 15, 4, 5, 0, time.UTC),
-				Comment:      "hogehoge",
-				PrefectureId: 1,
-				Longitude:    123.456,
-				Latitude:     123.456,
+			uploadPosts: []model.UploadPost{
+				{
+					Image:        "post1",
+					Date:         time.Date(2025, time.January, 30, 15, 4, 5, 0, time.UTC),
+					Comment:      "hogehoge1",
+					PrefectureId: 1,
+					Longitude:    123.456,
+					Latitude:     123.456,
+				},
+				{
+					Image:        "post2",
+					Date:         time.Date(2025, time.January, 31, 10, 10, 10, 0, time.UTC),
+					Comment:      "hogehoge2",
+					PrefectureId: 1,
+					Longitude:    124.456,
+					Latitude:     124.456,
+				},
+				{
+					Image:        "post3",
+					Date:         time.Date(2025, time.February, 1, 12, 0, 0, 0, time.UTC),
+					Comment:      "hogehoge3",
+					PrefectureId: 2,
+					Longitude:    125.456,
+					Latitude:     125.456,
+				},
 			},
-			want: model.Count{
-				Data: []model.CountPostByPrefectureId{
-					{
-						PrefectureId: 1,
-						PostCount:    1,
-					},
+			want: []model.PostsByPrefecture{
+				{
+					PrefectureId: 1,
+					PostIds:      []model.PostId{}, // 実際のPostIdはテスト中に取得
+				},
+				{
+					PrefectureId: 2,
+					PostIds:      []model.PostId{},
 				},
 			},
 		},
@@ -60,18 +80,31 @@ func TestCreateUserAndPostAndFetchPostCount(t *testing.T) {
 	for _, tc := range testCases {
 		tc := tc
 		t.Run(tc.name, func(t *testing.T) {
-			// ユーザーと投稿の作成
-			_, createdPost := testUtils.CreateUserAndPostForTest(t, userRepository, postRepository, tc.createUser, tc.uploadPost)
-
-			// 投稿数を取得
-			count, err := paintRepository.CountPostsByPrefecture(createdPost.GroupId)
-			if err != nil {
-				t.Fatalf("CountPostsByPrefecture() error = %v", err)
+			var createdPostIDs []model.PostId
+			for _, post := range tc.uploadPosts {
+				_, createdPost := testUtils.CreateUserAndPostForTest(t, userRepository, postRepository, tc.createUser, post)
+				createdPostIDs = append(createdPostIDs, createdPost.PostId)
 			}
 
-			// 結果を比較
-			if diff := cmp.Diff(tc.want, *count); diff != "" {
-				t.Errorf("CountPostsByPrefecture() mismatch (-want +got):\n%s", diff)
+			expectedResult := make(map[model.PrefectureId][]model.PostId)
+			for i, post := range tc.uploadPosts {
+				expectedResult[post.PrefectureId] = append(expectedResult[post.PrefectureId], createdPostIDs[i])
+			}
+
+			groupId := model.GroupId(1)
+			result, err := paintRepository.FindPostIDsByPrefecture(groupId)
+			if err != nil {
+				t.Fatalf("FindPostIDsByPrefecture() error = %v", err)
+			}
+
+			for i, expected := range tc.want {
+				if postIds, ok := expectedResult[expected.PrefectureId]; ok {
+					tc.want[i].PostIds = postIds
+				}
+			}
+
+			if diff := cmp.Diff(tc.want, result); diff != "" {
+				t.Errorf("FindPostIDsByPrefecture() mismatch (-want +got):\n%s", diff)
 			}
 		})
 	}
