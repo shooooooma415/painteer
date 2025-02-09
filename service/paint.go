b1fba7ca2ea2a0eda1af03a1cf81565398dceb58
shooooooma415 <shoma20040415@icloud.com>
2025-02-09 18:12:00 +0900

    feat:PaintServiceImplを定義

--- service/paint.go
+++ service/paint.go
@@ -1,2 +1,20 @@
 package service
 
+import (
+	"painteer/model"
+	"painteer/repository/paint"
+)
+
+type PaintService interface {
+	RegisterUser(user model.CreateUser) (*model.User, error)
+	AuthenticateUser(authId model.AuthId) (*model.User, error)
+	GetUserByID(userId model.UserId) (*model.User, error)
+}
+
+type PaintServiceImpl struct {
+	repo paint.PaintRepository
+}
+
+func NewPaintService(repo paint.PaintRepository) *PaintServiceImpl {
+	return &PaintServiceImpl{repo: repo}
+}
