5d13d242ba1c7406188b0390caa949c218c67809
shooooooma415 <shoma20040415@icloud.com>
2025-02-10 00:06:27 +0900

    feat:InitPostingRoutesの実装

---
+++ router/v1/posting.go
@@ -0,0 +1,14 @@
+package v1
+
+import (
+	"painteer/handler"
+	"painteer/service"
+
+	"github.com/labstack/echo/v4"
+)
+
+func InitPostingRoutes(e *echo.Echo, postingService service.PostingService, groupService service.GroupService, authService service.AuthService) {
+	e.POST("/post", handler.UploadPost(postingService, groupService))
+	e.GET("/post/map", handler.GetPosts(postingService))
+	e.GET("/post", handler.GetPost(postingService, authService))
+}
