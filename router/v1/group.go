dc2210b4e21d6e46dc80ad2140a20ef95233014e
shooooooma415 <shoma20040415@icloud.com>
2025-02-25 15:58:22 +0900

    fix: routerのimportを修正

--- router/v1/group.go
+++ router/v1/group.go
@@ -1,7 +1,7 @@
 package v1
 
 import (
-	"painteer/handler"
+	"painteer/handler/v1"
 	"painteer/service"
 
 	"github.com/labstack/echo/v4"
@@ -11,9 +11,9 @@ func InitGroupRoutes(e *echo.Echo, groupService service.GroupService, authServic
 
 	e.Group("/group")
 
-	e.POST("/", handler.RegisterGroup(groupService))
-	e.PUT("/member", handler.JoinGroup(groupService))
-	e.GET("/user", handler.GetUserGroup(groupService))
-	e.GET("/member", handler.GetGroupMembers(groupService,authService))
-	e.GET("/", handler.GetGroup(groupService))
+	e.POST("/", v1.RegisterGroup(groupService))
+	e.PUT("/member", v1.JoinGroup(groupService))
+	e.GET("/user", v1.GetUserGroup(groupService))
+	e.GET("/member", v1.GetGroupMembers(groupService,authService))
+	e.GET("/", v1.GetGroup(groupService))
 }
