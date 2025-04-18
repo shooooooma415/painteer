2aae93f7828c364498b7aaadfd537a1541adfc9b
shooooooma415 <shoma20040415@icloud.com>
2025-04-18 15:42:50 +0900

    fix: エラー箇所をコメントアウトしてbuildできるようにした

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module painteer
 
-go 1.23.3
+go 1.23
 
 require (
 	github.com/google/go-cmp v0.6.0 // indirect
--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"log"
 	userPostgresql"painteer/repository/auth/postgresql"
-	postPostgresql"painteer/repository/posting/postgresql"
+	// postPostgresql"painteer/repository/post/postgresql"
 	groupPostgresql"painteer/repository/group/postgresql"
 
 	"painteer/repository/utils"
@@ -24,16 +24,16 @@ func main() {
 	defer db.Close()
 
 	usersRepo := userPostgresql.NewAuthRepository(db)
-	postsRepo := postPostgresql.NewPostRepository(db)
+	// postsRepo := postPostgresql.NewPostRepository(db)
 	groupsRepo := groupPostgresql.NewGroupRepository(db)
 
 	authService := service.NewAuthService(usersRepo)
-	postingService := service.NewPostingService(postsRepo)
+	// postingService := service.NewPostingService(postsRepo)
 	groupService := service.NewGroupService(groupsRepo)
 
 	v1.InitAuthRoutes(e, authService)
 	v1.InitGroupRoutes(e, groupService,authService)
-	v1.InitPostingRoutes(e,postingService,groupService,authService)
+	// v1.InitPostingRoutes(e,postingService,groupService,authService)
 
 	log.Println("Starting server on :8080...")
 	e.Logger.Fatal(e.Start(":8080"))
