6ed028bb72b85e33d382315730bc6bec6ac27757
shooooooma415 <shoma20040415@icloud.com>
2025-04-19 15:11:05 +0900

    hotfix: サーバーが建つように修正

--- repository/utils/setupDB.go
+++ repository/utils/setupDB.go
@@ -20,11 +20,11 @@ func ConnectDB() (*sql.DB, error) {
 	}
 
 	// Fetch database connection information from environment variables
-	host := os.Getenv("DB_HOST")
-	port := os.Getenv("DB_PORT")
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	dbname := os.Getenv("DB_NAME")
+	host := os.Getenv("POSTGRES_HOST")
+	port := os.Getenv("POSTGRES_PORT")
+	user := os.Getenv("POSTGRES_USER")
+	password := os.Getenv("POSTGRES_PASSWORD")
+	dbname := os.Getenv("POSTGRES_DB")
 
 	// Validate the required environment variables
 	if host == "" || port == "" || user == "" || password == "" || dbname == "" {
@@ -33,7 +33,7 @@ func ConnectDB() (*sql.DB, error) {
 
 	// Create the Data Source Name (DSN)
 	dsn := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=require",
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname,
 	)
 
